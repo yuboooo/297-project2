140d6befa554741ff6dd20bbb2306d9112a76166
Yubo Jing <jybjybjing@YubodeMacBook-Air.local>
2022-04-16 19:20:43 -0400

    finish follow up in 297 guild

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// ./src/index.js
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// Imports
+import React from "react";
+import ReactDOM from "react-dom";
+// Components
+import App from "./App";
+// Styles
+import "./index.css";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
